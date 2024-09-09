a2ce3ed2f59398ac2a4ac6f298a258c9e1f6493e
Aliaksandr Fedukovich <1632637@mail.ru>
2024-09-09 08:54:07 +0300

    feat: JSON request and response

--- index.js
+++ index.js
@@ -4,11 +4,30 @@ const port = 3000;
 const host = '127.0.0.1';
 
 const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", 'text/plain');
-    res.statusCode = 200;
-    res.end('Hello world')
+    parseBodyJson(req, (err, payload) => {
+        const obj = {
+            c: Number(payload.a) + Number(payload.b)
+        }
+
+        res.setHeader("Content-Type", 'application/json');
+        res.statusCode = 200;
+        res.end(JSON.stringify(obj))
+    })
+
 });
 
 server.listen(port, host, () => {
     console.log(`server is available at ${host}:${port}`)
 })
+
+function parseBodyJson(req, cb) {
+    let body = [];
+
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = body.join('').toString();
+        const params = JSON.parse(body);
+        cb(null, params)
+    })
+}
