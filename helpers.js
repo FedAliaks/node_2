5d1be12c6b8095641e5b6c6bdd0eb90ba41e966e
Aliaksandr Fedukovich <1632637@mail.ru>
2024-09-09 08:58:22 +0300

    refact: refact code

---
+++ helpers.js
@@ -0,0 +1,16 @@
+function parseBodyJson(req, cb) {
+    let body = [];
+
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = body.join('').toString();
+        const params = JSON.parse(body);
+        cb(null, params)
+    })
+}
+
+
+module.exports = {
+    parseBodyJson
+}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const http = require('http');
+const helper = require('./helpers')
 
 const port = 3000;
 const host = '127.0.0.1';
 
 const server = http.createServer((req, res) => {
-    parseBodyJson(req, (err, payload) => {
+    helper.parseBodyJson(req, (err, payload) => {
         const obj = {
             c: Number(payload.a) + Number(payload.b)
         }
@@ -20,14 +21,3 @@ server.listen(port, host, () => {
     console.log(`server is available at ${host}:${port}`)
 })
 
-function parseBodyJson(req, cb) {
-    let body = [];
-
-    req.on('data', (chunk) => {
-        body.push(chunk);
-    }).on('end', () => {
-        body = body.join('').toString();
-        const params = JSON.parse(body);
-        cb(null, params)
-    })
-}
